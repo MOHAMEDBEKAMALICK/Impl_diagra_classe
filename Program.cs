488923319258eda1b6e7525567491cefe6d794d6
21264 <21264@HUAWEI>
2023-01-03 12:33:54 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+public class QD
+{ }
+public class Note
+{
+    private String Candidat;
+
+}
+
+
+
+
+public class Question
+{
+    private string Type;
+    private int Numero;
+    private int Point;
+}
+
+
+
+
+public class Etudiant
+{
+    private String Nom;
+    private int CNE;
+    private string Prénom;
+}
+
+public class Reponse
+{
+    private String Type;
+    private int Points;
+
+}
+public class Examen
+{
+    private int Note;
+    private string Matiére;
+    private int Date;
+
+}
+
+
+
+
+public class QCM
+{
+    private String question;
+
+}
